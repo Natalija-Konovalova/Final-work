82b512efe6dad7f954fc475ee23f515e58b121ee
Natalija Konovalova <natalja.konovalova@gmail.com>
2025-01-06 21:06:24 +0200

    Final commit

--- Main.js
+++ Main.js
@@ -1,41 +1,68 @@
-let min = 4;
-let max = 56;
+const button = document.querySelector(".button")
+const passwordElement = document.getElementById("password")
+const passwordParentElement = document.getElementById("password-parent")
+const passwordLengthElement = document.getElementById("password_length")
+const numbersCheckbox = document.getElementById("numbers_checkbox")
+const lettersCheckbox = document.getElementById("letters_checkbox")
+const specialSymbolsCheckbox = document.getElementById("special_symbols_checkbox")
 
-function generate_password() {
-    let password = "";
-       let length = document.querySelector("#password_length").value;
-let uppercase = document.querySelector("#uppercase").checked;
-let symbol = document.querySelector("#symbols").checked;
-let number = document.querySelector("#numbers").checked;
+let result = ""
+let passwordLength = "8"
 
-if (uppercase + symbol + number <= 0)
-return;
+passwordLengthElement.addEventListener("change", (e) => {
+    passwordLength = Number(e.target.value)
+})
 
-for (let i=0; i<length; i++) {
-const r = generator (0, 2);
-if(uppercase && r ===0) {
-    password += generateRandomUpperCase();
-} else if (symbol && r ===1) {
-    password += generateRandomSymbol();
-} else if (number && r ===2) {
-    password += generator(0, 9);
+button.addEventListener("click", () => {
+if(numbersCheckbox.checked) {
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+} else { numbers = [""]
+}
+if(lettersCheckbox.checked) {
+    latinSymbols = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+        "u", "v", "w", "x", "y", "z"
+    ]
+} else { latinSymbols = [""]
+}
+if(specialSymbolsCheckbox.checked) {
+    specialSymbols = [
+        "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "~"
+    ]
+} else { specialSymbols = [""]
+}
+
+const symbolsArray = [numbers, latinSymbols, specialSymbols]
+
+if(!numbersCheckbox.checked && !lettersCheckbox.checked && !specialSymbolsCheckbox.checked) {
+    alert("Password should include any symbols")
 } else {
-    i--;
+    while(result.length <= passwordLength) {
+        let isUpperCase = Math.round(Math.random() * 3) ==2
+        let currentArray = symbolsArray[Math.floor(Math.random() * symbolsArray.length)]
+        if(isUpperCase) {
+            result += currentArray[Math.floor(Math.random()* currentArray.length)].toString().toUpperCase()
+        } else {
+            result += currentArray[Math.floor(Math.random()* currentArray.length)]
+        }
+    }
 }
-document.querySelector("#result").textContent = password;
-}
-}
-generate_password();
+    passwordElement.innerHTML = result
+    result = ""
+ 
+    }
+    )
 
-function generateRandomUpperCase() {
-    return String.fromCharCode(generator(65, 90));
-}
-
-function generateRandomSymbol() {
-    const symbol = " ! @ # $ % ^ & * ( ) - _ = + \ | [ ] { } ; : / ? . >";
-    return symbol(generator(0, symbol.length - 1));
-}
-
-function generator() {
-    return Math.floor(Math.random() * (max + 1 - min) + min);
-}
+let copyText = document.querySelector(".settings2");
+copyText.querySelector(".buttonCopy").addEventListener("click", function() {
+    let input = copyText.querySelector("#password");
+    const textToCopy = input.textContent
+    .trim();
+   
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        alert("Password copied!");
+        input.textContent = "";
+        
+    })
+})
